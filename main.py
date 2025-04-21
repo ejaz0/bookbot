cad2656f9f32b557295b1fe3c1a3d9a7a1b62553
ThePrimeagen <the.primeagen@aol.com>
2025-04-21 21:17:15 +0200

    Completed bookbot

--- main.py
+++ main.py
@@ -1,13 +1,36 @@
-from stats import books_number_words
+from stats import books_number_words, num_of_characters, print_report
+import sys
 
 def get_books_text(path):
     with open(path) as f:
         return f.read()
 
 
+
 def main():
-    book_path = './books/frankenstein.txt'
-    text = get_books_text(book_path)
-    num_words = books_number_words(text)
-    print(f"{num_words} words found in the document")
+    if len(sys.argv) < 2:
+        print(f"Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    else:
+        book_path = sys.argv[1]
+        text = get_books_text(book_path)
+        num_words = books_number_words(text)
+        print(f"{num_words} words found in the document")
+        num_chars = num_of_characters(text)
+        print(num_chars)
+        sorted_chars = print_report(num_chars)
+        print("============ BOOKBOT ============")
+        print(f"Analazying book found at {book_path}...")
+        print("----------- Word Count ----------")
+        print(f"Found {num_words} total words")
+        print("--------- Character Count -------")
+        for char_dict in sorted_chars:
+            char = char_dict["char"]
+            count = char_dict["count"]
+            if char.isalpha():
+                print(f"{char}: {count}")
+        print("============= END ===============")
+
+
+
 main()
--- stats.py
+++ stats.py
@@ -2,3 +2,24 @@ def books_number_words(text):
     words = text.split()
     return len(words)
     
+def num_of_characters(text):
+    char_count = {}
+    lower_text = text.lower()
+    for char in lower_text:
+        if char in char_count:
+            char_count[char] += 1
+        else:
+            char_count[char] = 1
+    return char_count
+
+
+def sort_on(dict):
+    return dict["count"]
+
+def print_report(dict_chars):
+    sorted_list = []
+    for key, value in dict_chars.items():
+        char_dict = {"char": key, "count": value}
+        sorted_list.append(char_dict)
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
